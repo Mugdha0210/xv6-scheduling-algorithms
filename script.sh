4640b73655bef728c09219aac5a37956b9ba46a3
Mugdha Bapat <mugdhabapat2000@gmail.com>
2021-05-01 15:31:53 +0530

    script changed

--- script.sh
+++ script.sh
@@ -3,24 +3,28 @@ echo " "
 echo " "
 echo "        ------------        FIRST COME FIRST SERVE        ----------"
 echo " "
-git checkout multilevel-feedback-queue
+git checkout fcfs
+make clean
 make qemu
 echo " "
 echo " "
 echo "        ------------        PRIORITY SCHEDULING        ------------"
 echo " "
 git checkout priority-scheduling
+make clean
 make qemu
 echo " "
 echo " "
 echo "        ------------        MULTILEVEL QUEUE SCHEDULING        ------------"
 echo " "
 git checkout multilevel-queue
+make clean
 make qemu
 echo " "
 echo " "
 echo "        ------------        MULTILEVEL FEEDBACK QUEUE SCHEDULING        ----------"
 echo " "
 git checkout multilevel-feedback-queue
+make clean
 make qemu
 echo " "
